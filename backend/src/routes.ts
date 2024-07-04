208b7ba8212049fb9a0b2127801c4b90dfd332e6
Gaurav Rai <gauravrai568@gmail.com>
2024-07-04 19:50:05 -0400

    feat: added 2 get endpoints
    - Added `/questions` to fetch all questions at once as is.
    - Added `/question/random` to fetch one random question at a time.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,22 @@
 import { Router } from 'express';
+import questions from './questions.json';
 
 const router = Router();
 
 // Health check
 router.get('/health', (req, res) => {
   res.send('OK');
+});
+
+router.get('/questions', (req, res) => {
+    res.json(questions);
+  });
+
+router.get('/questions/random', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * questions.length);
+    const randomQuestion = questions[randomIndex];
+    const { id, question, options } = randomQuestion;
+    res.json({ id, question, options });
 });
 
 export default router;
